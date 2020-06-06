ebac1c5be1c9f9d3a8a8ba2fc3481f978147dec2
whitfield-lyx <10842089@qq.com>
2020-06-06 10:50:55 +0800

    ignore了一个vcs

--- src/main/java/com/example/ecnu_time_bank/mapper/FacilityMapper.java
+++ src/main/java/com/example/ecnu_time_bank/mapper/FacilityMapper.java
@@ -4,14 +4,9 @@ import com.example.ecnu_time_bank.entity.Facility;
 
 public interface FacilityMapper {
     int deleteByPrimaryKey(Integer facilityId);
-
     int insert(Facility record);
-
     int insertSelective(Facility record);
-
     Facility selectByPrimaryKey(Integer facilityId);
-
     int updateByPrimaryKeySelective(Facility record);
-
     int updateByPrimaryKey(Facility record);
 }
